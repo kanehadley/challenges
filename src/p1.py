858e169ac4ba72d73e79b83a9dd6df5a9685ca28
Kane Hadley <kanehadley@Prometheus.local>
2015-01-28 11:24:25 -0500

    Adding problem 1 solution and ipython/

---
+++ src/p1.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+def p1(n):
+    ''' Sums the numbers between 0 and n that are multiples of 3 and 5. '''
+    return sum([x for x in range(0, n) if x % 3 == 0 or x % 5 == 0])
