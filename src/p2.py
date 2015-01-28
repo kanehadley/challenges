4faba812d54d68dcceb9f37f04b719449183016b
Kane Hadley <Kane Hadley>
2015-01-28 14:03:51 -0500

    Added comments explaining what the solution is doing.

--- src/p2.py
+++ src/p2.py
@@ -1,4 +1,8 @@
 def p2 ():
+    '''
+        Iterates over all the fibonnaci numbers less than 4 million and sums them
+        up. Next time try a python Generator instead of this iterative loop.
+    '''
     total = 0
     previous = 1
     current = 1
