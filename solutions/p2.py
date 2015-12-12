01480a45d7ca2c30b5b88525c1bab48b923c79e0
Kane Hadley <Kane Hadley>
2015-12-12 01:10:22 -0500

    Created a fibonnaci generator.

--- solutions/p2.py
+++ solutions/p2.py
@@ -1,19 +1,18 @@
-def p2 ():
-    '''
-        Iterates over all the fibonnaci numbers less than 4 million and sums them
-        up. Next time try a python Generator instead of this iterative loop.
-    '''
-    total = 0
+#! /usr/bin/env python
+
+import itertools
+
+def fibonnaci ():
     previous = 1
     current = 1
 
-    while current < 4000000:
-        if current % 2 == 0:
-            total += current
+    while True:
+        yield current
         temp = current
         current += previous
         previous = temp
 
-    print total
+def p2 ():
+    print(sum(filter(lambda x: x % 2 == 0, itertools.takewhile(lambda x: x < 4000000, fibonnaci()))))
 
 
