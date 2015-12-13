5738263e4bfd3331791bbc5e91f3827c3af6da7a
Kane Hadley <Kane Hadley>
2015-12-12 22:48:26 -0500

    More tools.

--- solutions/toolset.py
+++ solutions/toolset.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 from itertools import dropwhile, filterfalse
-from math import sqrt, floor
+from math import sqrt, floor, ceil
 
 
 def fibonnaci_generator():
@@ -62,3 +62,38 @@ def is_palindrome_int(n):
                                 map(lambda x, y: x == y,
                                     str(n),
                                     reversed(str(n)))))) == 0
+
+
+def factorize(n, factors_superset):
+    """
+
+    :param n: Integer to be factored.
+    :param factors: List of possible integers that can divide n.
+    :return: Factorization of n based on the values in factors. May have
+        duplicates of some factors.
+    """
+    factors = filter(lambda x: 1 != x, factors_superset)
+    start = list(dropwhile(lambda x: n % x != 0, sorted(factors)))
+    if 0 == len(start):
+        return [int(n)]
+    else:
+        return [start[0]] + factorize(int(n / start[0]), factors_superset)
+
+
+def prime_factorization(n):
+    """Finds the prime factors for an integer, n
+
+    :param n: Integer to factor.
+    :return: list - The prime factors for the integer
+    """
+
+    # Grab all possible prime factors
+    primes = list(filter(lambda x: isprime(x), range(2, ceil(sqrt(n)) + 1)))
+
+    if 1 == n:
+        return [1]
+
+    if isprime(n):
+        return [1, n]
+
+    return factorize(n, primes)
